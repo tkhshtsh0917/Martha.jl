415f10ee418a9231964253f6ac2affe541e25606
tkhshtsh0917 <48384135+tkhshtsh0917@users.noreply.github.com>
2022-04-10 00:45:34 +0900

    feat: Added  function.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Martha
 using Test
 
-@testset "Martha.jl" begin
+@testset "Fuctional tests" begin
     # Write your tests here.
+    @test adder(1, 2) == 3
+    @test adder(-3, -4) == -7
+    @test adder(5, -6) == -1
 end
