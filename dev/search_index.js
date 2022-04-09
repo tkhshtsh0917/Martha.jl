370568007c35a80b039269b4edb1248ad6e1e2d9
Documenter.jl <documenter@juliadocs.github.io>
2022-04-09 14:23:57 +0000

    build based on 5c608a8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Martha","category":"page"},{"location":"#Martha","page":"Home","title":"Martha","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Martha.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Martha]","category":"page"}]
+}
